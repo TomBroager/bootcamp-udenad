684706e689495a804b209a42c43f0d71db45d7b2
TomBroager <U773553@rts-365.dk>
2021-08-17 14:33:15 +0200

    add solution assignment 01b

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -1,6 +1,32 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	let number = 5;
+	let maxNumber = 10;
+	let minNumber = 0;
+	let plusBtn = document.querySelector('#btn-count-increase');
+	let minusBtn = document.querySelector('#btn-count-decrease');
+	let textCountElement = document.querySelector('#text-count');
+
+	
+	function updateNumber() {
+		textCountElement.textContent = number;
+	};
+
+	updateNumber();
+	
+	plusBtn.addEventListener('click', () => {
+		if(number < maxNumber){
+			number++;
+			updateNumber();
+		};
+	});
+	
+	minusBtn.addEventListener('click', () => {
+		if(number > minNumber){
+			number--;
+			updateNumber();
+		};
+	});
 
 }); // Afslutter: DOMContentLoaded
