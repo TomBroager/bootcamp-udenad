a549f28cd9f7425f2226b3456c7aef81c345e969
TomBroager <U773553@rts-365.dk>
2021-08-19 11:25:49 +0200

    add solution to assigment 02b

--- eks_02b_funktioner/js/index.js
+++ eks_02b_funktioner/js/index.js
@@ -1,6 +1,14 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	function hils(navn){
+		console.log('Hej med dig, ' + navn);
+		console.log(`Hej med dig, ${navn}`); // anbefalede måde at indsætte parameter på
+		console.log(`Hej med dig,
+		
+		${navn}`);
+	};
+	
+	hils('Tom');
 
 }); // Afslutter: DOMContentLoaded
